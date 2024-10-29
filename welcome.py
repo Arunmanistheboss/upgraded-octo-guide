af8168dab43de9b7d1548dd940e47ebd605066f5
Arun <arun2013ane@gmail.com>
2024-10-29 10:37:00 +0100

    Add python file

---
+++ welcome.py
@@ -0,0 +1,6 @@
+if __name__ =="__main__":
+    saisie = input("Entrer oui pour validder").lower().strip()
+    if saisie == "oui":
+        print("Merci")
+    else:
+        print("Je ne comprend pas")
